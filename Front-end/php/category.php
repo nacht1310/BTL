e1685f5790dbdf2b866f8d345199305b1b786f81
Nacht <ducvt1310@gmail.com>
2022-12-07 16:17:38 +0700

    Category Page 90%

--- Front-end/php/category.php
+++ Front-end/php/category.php
@@ -2,27 +2,30 @@
 
 <?php 
     $db = new Database();
-    $query = "SELECT 'name' FROM 'tbl_brand'";
+    $query = "SELECT name FROM tbl_brand";
     $result = $db->select($query);
 
-    while($value = $result->fetch_assoc()) {
-        $brand = $value;
+    for($i = 0; $i < $result->num_rows; $i++) {
+        $brand[$i] = $result->fetch_assoc();
     }
 
-    $query = "SELECT 'name' FROM 'tbl_model'";
+    $query = "SELECT name FROM tbl_model";
     $result = $db->select($query);
 
-    while($value = $result->fetch_assoc()) {
-        $model = $value;
+    for($i = 0; $i < $result->num_rows; $i++) {
+        $model[$i] = $result->fetch_assoc();
     }
 
-    $query = "SELECT 'year' FROM 'tbl_year'";
+
+    $query = "SELECT year FROM tbl_year";
     $result = $db->select($query);
 
-    while($value = $result->fetch_assoc()) {
-        $year = $value;
+    for($i = 0; $i < $result->num_rows; $i++) {
+        $year[$i] = $result->fetch_assoc();
     }
+
 ?>
+
 
 <?php include "./header.php" ?>
 
@@ -93,7 +96,7 @@
         
     </div>
 
-    <form class="filter-board">
+    <form class="filter-board" action="category.php" method="POST">
         <div style="display: flex; width:100%; height:85%">
             <div class="menu">
                 <div class="menu-item" id="brand-menu">
@@ -115,20 +118,160 @@
             <div class="board" id="brand-board">
                 <h3>Brand</h3>
                 <h4>All Brand</h4>
+
+                <div class="row">
+                    <?php 
+                    $n= floor(count($brand)/3);
+                        echo '<div style="width:33%">';
+                        for($i = 0; $i < $n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="brand" value="';
+                            echo $brand[$i]["name"];
+                            echo '">
+                                <label for="brand">';
+                            echo $brand[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = $n+1; $i < 2*$n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="brand" value="';
+                            echo $brand[$i]["name"];
+                            echo '">
+                                <label for="brand">';
+                            echo $brand[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = 2*$n+1; $i < count($brand); $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="brand" value="';
+                            echo $brand[$i]["name"];
+                            echo '">
+                                <label for="brand">';
+                            echo $brand[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+                    ?>
+                </div>
             </div>
             <div class="board" id="model-board">
                 <h3>Model</h3>
                 <h4>All Model</h4>
+
+                <div class="row">
+                    <?php 
+                    $n= floor(count($model)/3);
+                        echo '<div style="width:33%">';
+                        for($i = 0; $i < $n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="model" value="';
+                            echo $model[$i]["name"];
+                            echo '">
+                                <label for="model">';
+                            echo $model[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = $n+1; $i < 2*$n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="model" value="';
+                            echo $model[$i]["name"];
+                            echo '">
+                                <label for="model">';
+                            echo $model[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = 2*$n+1; $i < count($model); $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="model" value="';
+                            echo $model[$i]["name"];
+                            echo '">
+                                <label for="model">';
+                            echo $model[$i]["name"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+                    ?>
+                </div>
             </div>
             <div class="board" id="year-board">
                 <h3>Year</h3>
-                <h4>All Year</h4>
-                <input type="checkbox">
+
+                <div class="row">
+                    <?php 
+                    $n= floor(count($year)/3);
+                        echo '<div style="width:33%">';
+                        for($i = 0; $i < $n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="year" value="';
+                            echo $year[$i]["year"];
+                            echo '">
+                                <label for="year">';
+                            echo $year[$i]["year"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = $n+1; $i < 2*$n+1; $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="year" value="';
+                            echo $year[$i]["year"];
+                            echo '">
+                                <label for="year">';
+                            echo $year[$i]["year"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+
+                        echo '<div style="width:33%">';
+                        for($i = 2*$n+1; $i < count($year); $i++) {
+                            echo '<div class="check">
+                                <input type="checkbox" name="year" value="';
+                            echo $year[$i]["year"];
+                            echo '">
+                                <label for="year">';
+                            echo $year[$i]["year"];
+                            echo '</label>
+                            </div>
+                            ';
+                        }
+                        echo '</div>';
+                    ?>
+                </div>
             </div>
             <div class="board" id="price-board">
                 <h3>Price</h3>
 
             </div>
+            
         </div>
         <div style="display: flex; flex-direction: row-reverse;padding:20px; height:15%">
             <button class="btn" type="submit">Apply Filter(s)</button>
