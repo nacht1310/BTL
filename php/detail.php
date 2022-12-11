4d33f0b8460c784df67a496d6f5f4453fcbbb2d8
Thong101 <thong.hoang091199@hcmut.edu.vn>
2022-12-11 17:38:44 +0700

    merge code duc

--- php/detail.php
+++ php/detail.php
@@ -1,4 +1,13 @@
 <?php include "./header.php" ?>
+
+<?
+	if (!isset($_GET['id'])) {
+		header('location: shop.php');
+	} else {
+		$idSP = $_GET['id'];
+	}
+?>
+
 <div class="container"> 
     <div class = "card-wrapper">
       <div class = "card">
@@ -6,31 +15,31 @@
         <div class = "product-imgs">
           <div class = "img-display">
             <div class = "img-showcase">
-              <img src = "img/m326238-0009.webp" alt = "sky-dwell image">
-              <img src = "img/m326934-0003.webp" alt = "sky-dwell image">
-              <img src = "img/m326935-0005.webp" alt = "sky-dwell image">
-              <img src = "img/m326938-0005.webp" alt = "sky-dwell image">
+              <img src = "../images/product/<?php echo $row['img']; ?>" alt = "watchs image">
+              <img src = "../images/product/<?php echo $row['img1']; ?>" alt = "watchs image">
+              <img src = "../images/product/<?php echo $row['img2']; ?>" alt = "watchs image">
+              <img src = "../images/product/<?php echo $row['img3']; ?>" alt = "watchs image">
             </div>
           </div>
           <div class = "img-select">
             <div class = "img-item">
               <a href = "#" data-id = "1">
-                <img src = "img/m326238-0009.webp" alt = "sky-dwell image">
+                <img src = "../images/product/<?php echo $row['img']; ?>" alt = "watchs image">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "2">
-                <img src = "img/m326934-0003.webp" alt = "sky-dwell image">
+                <img src = "../images/product/<?php echo $row['img1']; ?>" alt = "watchs image">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "3">
-                <img src = "img/m326935-0005.webp" alt = "sky-dwell imagee">
+                <img src = "../images/product/<?php echo $row['img2']; ?>" alt = "watchs imagee">
               </a>
             </div>
             <div class = "img-item">
               <a href = "#" data-id = "4">
-                <img src = "img/m326938-0005.webp" alt = "sky-dwell image">
+                <img src = "../images/product/<?php echo $row['img3']; ?>" alt = "watchs image">
               </a>
             </div>
           </div>
@@ -39,30 +48,16 @@
         <div class = "product-content">
           <h2 class = "product-title">SKY-DWELLER</h2>
           <a href = "#" class = "product-link">visit Rolex store</a>
-          <div class = "product-rating">
-            <i class = "fas fa-star"></i>
-            <i class = "fas fa-star"></i>
-            <i class = "fas fa-star"></i>
-            <i class = "fas fa-star"></i>
-            <i class = "fas fa-star-half-alt"></i>
-            <span>4.7(21)</span>
-          </div>
 
           <div class = "product-price">
-            <p class = "last-price">Price: $25.700</p>
+           <p class = "last-price">Price: $price = <?php $row['GIASP']; ?> </p>
           </div>
 
           <div class = "product-detail">
             <h2>About this watch: </h2>
             <p>Rolex presents an 18 kt yellow gold version of its Oyster Perpetual Sky-Dweller, fitted with an Oysterflex bracelet. The watch is the first in the Classic category to include this innovative bracelet made of high-performance elastomer. It also features a bright black, sunray-finish dial with hands and hour markers in 18 kt yellow gold. The light reflections on the case sides and lugs highlight the refined profile of the 42 mm Oyster case.</p>
             <ul>
-            <li>  <label for="watch">Choose a Model:</label>
-                  <select name="watcs" id="watcs">
-                         <option value="Yellow Gold">Black Gold</option>
-                         <option value="Oystersteel and white gold">Oystersteel and white gold</option>
-                         <option value="Everose Gold">Everose Gold</option>
-                         <option value="Yellow Gold">Yellow Gold</option>
-                         </select></li>
+            <li> Model: <span>Gold Yellow</span></li>
               <li>Available: <span>in stock</span></li>
               <li>Category: <span>Classic Watch</span></li>
               <li>Shipping Area: <span>All over the world</span></li>
